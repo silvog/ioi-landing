d1692b825b37f046de49d65d685f17c8ed2720a8
silvog <silvo.gazvoda@cloud.si>
2016-06-09 22:33:13 +0200

    added modal with video element

--- js/layout.js
+++ js/layout.js
@@ -4,5 +4,27 @@ function scrollToTop() {
     }, 'slow');
 }
 
+$(document).ready(function () {
+    /*$('.modal').each(function(){
+     var src = $(this).find('iframe').attr('src');
 
+     $(this).on('click', function(){
 
+     $(this).find('iframe').attr('src', '');
+     $(this).find('iframe').attr('src', src);
+
+     });
+     });*/
+
+    /*$("#videoModalPreslikave").on('hidden.bs.modal', function (e) {
+     $("#videoModalPreslikave iframe").attr("src", $("#videoModalPreslikave iframe").attr("src"));
+     });*/
+
+    $("#videoModalPreslikave").on('hidden.bs.modal', function (e) {
+        $("#videoModalPreslikave video").attr("src", $("#videoModalPreslikave video").attr("src"));
+    });
+
+    $("#videoModalRazsiritve").on('hidden.bs.modal', function (e) {
+        $("#videoModalRazsiritve video").attr("src", $("#videoModalRazsiritve video").attr("src"));
+    });
+});
